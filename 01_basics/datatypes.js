c6a5afe358fd85c5b758857618ffbad5077778da
AshishAnant17 <141263763+AshishAnant17@users.noreply.github.com>
2024-08-31 12:35:37 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,1 +1,28 @@
 "use strict"//it will treat it asd a newer version
+console.log(3+3)
+//code readibilty should be high always 
+
+console.log("Hitesh")
+
+let name="Ashish"
+let age=19;
+let isbooleanvalue=true
+let state
+
+//primitive datatype
+//integer max value is 2 power of 53
+//for bigger int you should use bigint working for google and microsoft
+//string should be in "" for better understanding
+//boolean =true/false
+//null=standalone value
+//undefined
+//symbol=unique
+//another datatype
+//object
+console.log(typeof "Hitesh");
+console.log(typeof 123);
+console.log(typeof null) //object that is language problem
+console.log(typeof undefined)
+
+
+
