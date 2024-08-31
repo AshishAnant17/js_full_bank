d58bdbed70f8c1b675a671c874c00660cd4d67fc
AshishAnant17 <141263763+AshishAnant17@users.noreply.github.com>
2024-08-31 13:04:23 +0000

    conversion

---
+++ 01_basics/01_03_conversionOperation.js
@@ -0,0 +1,46 @@
+let score=33
+/*console.log(typeof score)
+console.log(typeof(score));
+
+these are the two way by which you can represent the type of variable
+*/
+
+let score_second="Hitesh"
+//type conversion
+let value_in_number=Number(score_second);
+console.log(typeof(value_in_number));
+
+/*
+now what are the conversions number do 
+input  output 
+"33"    33
+"33abc" NaN(iska type hai yy that is Not A Number)
+null    0
+notdefined 
+true 1
+false 0
+*/
+
+let booleanchecking=1
+let booleancheck=Boolean(booleanchecking)
+console.log(typeof(booleancheck));
+/*
+now some famous output input for boolean
+input output
+0   false
+1   true
+""  false
+"Hitesh" true
+
+
+*/
+
+let somenumber=33
+let stringconvertednumber=String(somenumber)
+console.log(typeof(stringconvertednumber))
+
+
+
+
+
+
