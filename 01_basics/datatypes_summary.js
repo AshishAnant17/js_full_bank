91c96ad0ba82ceac7061500ca8c5af9364a418de
AshishAnant17 <141263763+AshishAnant17@users.noreply.github.com>
2024-09-01 11:57:04 +0000

    datatypes_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -13,10 +13,10 @@ let usermail
 const id=Symbol('123')
 const currentid=Symbol('123')
 
-console.log(id===currentid)
+//console.log(id===currentid)
 
 const bigNumber=123479489739387n
-console.log(typeof bigNumber)
+//console.log(typeof bigNumber)
 //In JavaScript, each time you create a Symbol, even with the same description, it generates a unique value.
 
 
@@ -24,7 +24,7 @@ console.log(typeof bigNumber)
 //Array,function,object
 
 let newarray=[1,2,2,34,5,6]
-console.log(typeof newarray)// return object
+//console.log(typeof newarray)// return object
 
 let myobject={
     name:"Ashish",
@@ -35,4 +35,29 @@ let myfunction=function(){
     console.log("hello world")
 }
 myfunction();//output hello world
-console.log(myfunction);//output [Function: myfunction] output if function object that is its type 
+//console.log(myfunction);//output [Function: myfunction] output if function object that is its type
+
+
+/////////////////
+//primitive(stack)  and non premitive me heap kaam karega 
+//in stack we get the copy of the variable so the change is made to the copy 
+
+let mygmail="Ashishgoogledotcom"
+let newgmail=mygmail
+newgmail="pappugoogledotcom"
+//console.log(mygmail)//Ashishgoogledotcom
+//console.log(newgmail)//pappugoogledotcom
+
+
+//whereas in heap we get the original reference to the new variable also so lets check 
+let userone={
+    email:"user@googledotcom",
+    upi:"user@ybl"
+}
+let usertwo=userone;
+usertwo.email="harshthakur@gmail.com"
+console.log(userone.email)//harshthakur@gmail.com
+console.log(usertwo.email)//harshthakur@gmail.com
+//in heap original value also get changed
+
+
