dbb1ac2b82bcd46f65a03347e92582b8aeb28410
AshishAnant17 <141263763+AshishAnant17@users.noreply.github.com>
2024-09-05 17:19:59 +0000

    immediatelyInvikedFunction

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+//immediately invoked function expression
+
+//syntax({})();
+
+(function chai(){
+    console.log("ashish")
+})();
+
+((name)=>{
+    console.log(`Db connected ${name}`)
+})("center")
