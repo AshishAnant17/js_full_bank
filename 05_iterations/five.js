96279b567329d8ec80d2f7d83bf3aa03397fdc75
AshishAnant17 <141263763+AshishAnant17@users.noreply.github.com>
2024-09-06 07:42:10 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,34 @@
+//for each loop
+
+const coding=["js","ruby","java","python","css"]
+//forEach isko length batane ki koi jaruart nahi hai it will manage on its own 
+//isko call backfunction dena padega syntax function (function name nahi likhna hai so function (item ,ye travel karega ) {})
+coding.forEach(function (item){
+    //console.log(item);
+})
+
+//now using arrow vv traverse kar skte hai 
+
+coding.forEach((item)=>{
+    //console.log(item)
+})
+
+//now the condition when you are getting object inside the array 
+
+const myArray=[
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+myArray.forEach((item)=>{
+    console.log(item.languageName)
+})
